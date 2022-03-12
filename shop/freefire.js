3ea6f3616be5498e1c3d7e91e1e7f09c85d79bf9
Yyun <94148707+Yunsilstore@users.noreply.github.com>
2022-03-12 09:58:23 +0700

    Update freefire.js

--- shop/freefire.js
+++ shop/freefire.js
@@ -5,7 +5,7 @@ PROSES	1-180Mnt 1X24 Jam
 OPEN	10.00-19.00			
 FORMAT	ID			
 
-LIST DIAMOND VIA ID BY YUSNIL
+LIST DIAMOND VIA ID BY YUNSIL
 ⑅⠀ ⠀⑅⠀⠀ ⑅⠀ ⑅ ⑅⠀⠀ ⑅ 
 𝟧 💎 ➭ 𝟩𝟪𝟢
 𝟤𝟢 💎 ➭ 𝟤.𝟨𝟢𝟢 
@@ -15,12 +15,12 @@ LIST DIAMOND VIA ID BY YUSNIL
 𝟣𝟦𝟢 💎 ➭ 𝟣𝟩.50𝟢
 𝟤𝟣𝟢 💎 ➭ 𝟤6.00𝟢
 𝟥𝟧𝟧 💎 ➭ 𝟦3.𝟫𝟢𝟢
-𝟧𝟢𝟢 💎 ➭ 𝟨1.𝟪0𝟢
-𝟩𝟤𝟢 💎 ➭ 𝟪6.0𝟢𝟢
+𝟧𝟢𝟢 💎 ➭ 62.000
+𝟩𝟤𝟢 💎 ➭ 87.000
 𝟣𝟢𝟢𝟢 💎 ➭ 𝟣𝟤5.00𝟢
 𝟤𝟢𝟢𝟢 💎 ➭ 𝟤𝟥7.𝟢𝟢𝟢
 
-🥝 ࣪ ͎ ᎷᎷ ➭ 𝟤𝟨.5𝟢𝟢
+🥝 ࣪ ͎ ᎷᎷ ➭ 27.000
  🥝 ࣪ ͎ ᎷᏰ ➭ 𝟣𝟥1.5𝟢𝟢
 				
 Untuk pay / pembayan silahkan ketik ${prefix}pay
